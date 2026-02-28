519e5eb6e47bbdfc28d09b967c373b26d1100c6b
Sahil <sahilagicha19@gmail.com>
2026-02-28 18:11:00 +0530

    Update API keys to use placeholders
    Replaced actual API keys with placeholder values.

--- lib/constants/api_keys.dart
+++ lib/constants/api_keys.dart
@@ -1,3 +1,3 @@
-const String newsApiKey = 'd7b58ae4d4754df583e78e92879f7f94';
-const String footballApiKey = '67f7e18c172fb1e3b55fce3610af88d4';
-const String cricketApiKey = '5fe5d118-bffb-41de-8f66-31f820caa504';
+const String newsApiKey = 'YOUR-API_KEY';
+const String footballApiKey = 'YOUR-API_KEY';
+const String cricketApiKey = 'YOUR-API_KEY';
